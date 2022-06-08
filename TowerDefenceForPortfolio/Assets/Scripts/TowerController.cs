498f378f52e93979e1cc30591681fef6463b9fca
Ihor Novytskyi <inovitskiy13@gmail.com>
2022-06-08 09:18:06 +0300

    2

--- TowerDefenceForPortfolio/Assets/Scripts/TowerController.cs
+++ TowerDefenceForPortfolio/Assets/Scripts/TowerController.cs
@@ -18,36 +18,30 @@ public class TowerController : MonoBehaviour
 
     private GameObject[] towers;
     private GameObject[,] panels;
+    private int[,] turrelTypes;
 
-    public GameObject collider1;
-
-    private Camera cam;
+    private int currentTowerSide;
 
     // Start is called before the first frame update
     private void Awake()
     {
         towers = new GameObject[towerColliders.Length];
         panels = new GameObject[towerColliders.Length, 2];
-
-        //cam = GetComponent<Camera>();
+        turrelTypes = new int[2, towerColliders.Length];
 
         for(int i = 0; i < towerColliders.Length; i++)
         {
-            //Debug.Log(towerColliders[i].transform.position);
-            //Debug.Log(towerColliders[i].transform.localPosition);
-            Debug.Log(Camera.main.WorldToScreenPoint(towerColliders[i].transform.position));
-            Debug.Log("_____");
-
+            var screenPosition = Camera.main.WorldToScreenPoint(towerColliders[i].transform.position);
+            
             panels[i, 0] = Instantiate(BuyPanel, Canvas.transform, false);
             var rectTransform = panels[i, 0].GetComponent<RectTransform>();
-            rectTransform.position = Camera.main.WorldToScreenPoint(towerColliders[i].transform.position);//new Vector3(towerColliders[i].transform.position.x, towerColliders[i].transform.position.y, 0));
-            Debug.Log(rectTransform.position);
-            Debug.Log("gggggg");
+            rectTransform.position = new Vector3(screenPosition.x, screenPosition.y + panelPositionDistance);
+            panels[i, 0].SetActive(false);
 
             panels[i, 1] = Instantiate(ChangePanel, Canvas.transform, false);
             var rectTransform2 = panels[i, 1].GetComponent<RectTransform>();
-            rectTransform2.position = Camera.main.WorldToScreenPoint(towerColliders[i].transform.position);//Camera.main.WorldToScreenPoint(new Vector3(towerColliders[i].transform.position.x, towerColliders[i].transform.position.y, 0));
-
+            rectTransform2.position = new Vector3(screenPosition.x, screenPosition.y + panelPositionDistance);//Camera.main.WorldToScreenPoint(new Vector3(towerColliders[i].transform.position.x, towerColliders[i].transform.position.y, 0));
+            panels[i, 1].SetActive(false);
         }
     }
 
@@ -67,18 +61,75 @@ public class TowerController : MonoBehaviour
             {
                 if (hit.collider.tag == "TowerSide")
                 {
+                    for (int i = 0; i < towerColliders.Length; i++)
+                    {
+                        if (towerColliders[i] == hit.collider)
+                        {
+                            if (towers[i] == null)
+                            {
+                                if (!panels[i, 0].activeSelf)
+                                    panels[i, 0].SetActive(true);
+                                else if (panels[i, 0].activeSelf)
+                                    panels[i, 0].SetActive(false);
+                            }
+                            else if (towers[i] != null)
+                            {
+                                if (!panels[i, 1].activeSelf)
+                                    panels[i, 1].SetActive(true);
+                                else if (panels[i, 1].activeSelf)
+                                    panels[i, 1].SetActive(false);
+                            }
+
+                            currentTowerSide = i;
+
+                            break;
+                        }
+                    }
+
                     //if (!EventSystem.current.IsPointerOverGameObject())
-                        Instantiate(turrel1[0], new Vector3(hit.collider.transform.position.x, hit.collider.transform.position.y, 0), Quaternion.Euler(0, 0, 180));
+                        //Instantiate(turrel1[0], new Vector3(hit.collider.transform.position.x, hit.collider.transform.position.y, 0), Quaternion.Euler(0, 0, 180));
                 }
             }
         }
 
         if (Input.GetMouseButtonDown(0))
         {
-            var panel = Instantiate(BuyPanel, Canvas.transform, false);//, Vector3.zero, Quaternion.identity);
-            var rectTransform = panel.GetComponent<RectTransform>();
-            rectTransform.position = Input.mousePosition;
-            Debug.Log(Input.mousePosition);
+            //var panel = Instantiate(BuyPanel, Canvas.transform, false);//, Vector3.zero, Quaternion.identity);
+            //var rectTransform = panel.GetComponent<RectTransform>();
+            //rectTransform.position = Input.mousePosition;
+            //Debug.Log(Input.mousePosition);
         }
+
+    }
+
+    public void Buy(int trackNum)
+    {
+        var currentTowerSidePosition = towerColliders[currentTowerSide].transform.position;
+
+        if (trackNum == 0)
+        {
+            towers[currentTowerSide] = Instantiate(turrel1[0], new Vector3(currentTowerSidePosition.x, currentTowerSidePosition.y, 0), Quaternion.Euler(0, 0, 180));
+        }
+        else if (trackNum == 1)
+        {
+            towers[currentTowerSide] = Instantiate(turrel2[0], new Vector3(currentTowerSidePosition.x, currentTowerSidePosition.y, 0), Quaternion.Euler(0, 0, 180));
+        }
+
+        turrelTypes[trackNum ,currentTowerSide] = 0;
+        panels[currentTowerSide, 0].SetActive(false);
+    }
+
+    public void Upgrade()
+    {
+        if (towers[currentTowerSide] == turrel1[0])
+            Debug.Log("ffff");
+    }
+
+    public void Sale()
+    {
+        Destroy(towers[currentTowerSide]);
+        towers[currentTowerSide] = null;
+
+        panels[currentTowerSide, 1].SetActive(false);
     }
 }
